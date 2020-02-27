4cd52aa0837c6941f637b2c9b907b01f3de3c667
oswaldof4 <ofelix@ucsb.edu>
2020-02-27 11:18:35 -0800

    First push

---
+++ app.r
@@ -0,0 +1,43 @@
+# Second example!!!
+
+library(shiny)
+library(tidyverse)
+library(shinythemes)
+
+ui <- navbarPage("Waldo's naviagation bar!",
+                 theme = shinytheme("cyborg"),
+                 tabPanel("First tab!",
+                          h1("Some giant text"),
+                          p("Here's some regular text..."),
+                          plotOutput(outputId = "diamond_plot")),
+                 tabPanel("Second tab!",
+                          sidebarLayout(
+                            sidebarPanel("Some text!",
+                                         checkboxGroupInput(inputId = "diamondclarity",
+                                                            "Choose some!",
+                                                            choices = c(levels(diamonds$clarity)))
+                            ),
+                            mainPanel("Main panel text!",
+                                      plotOutput(outputId = "diamond_plot2"))
+                          ))
+)
+
+server <- function(input, output){
+  
+  output$diamond_plot <- renderPlot({
+    ggplot(data = diamonds, aes(x = carat, y = price)) +
+      geom_point(aes(color = clarity)) 
+  })
+  
+  diamond_clarity <- reactive({
+    diamonds %>% 
+      filter(clarity %in% input$diamondclarity)
+  })
+  
+  output$diamond_plot2 <- renderPlot({
+    ggplot(data = diamond_clarity(), aes(x = clarity, y = price)) +
+      geom_violin(aes(fill = clarity))
+  })
+}
+
+shinyApp(ui = ui, server = server)
